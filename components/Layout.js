9d16fff85ac79c2a2917d19829d6231570493dd6
James Chan <108029209+j-chany@users.noreply.github.com>
2023-02-23 18:21:40 -0800

    routing from menu finished

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 const Layout = (props) => {
   const { children } = props;
 
--- components/Menu.js
+++ components/Menu.js
@@ -1,8 +1,16 @@
 import React from "react";
-
+import { useRouter } from "next/router";
 import styles from "../styles/home.module.css";
 
 const Menu = () => {
+  // create router object 
+  const router = useRouter();
+
+  // on each buton click, redirect to the new route which will be passed in according to the button
+  const handleClick = (route) => {
+    router.push(`/${route}`);
+  };
+
   return (
     <main className="flex justify-center">
       <div className={styles.menu}>
@@ -14,13 +22,22 @@ const Menu = () => {
           Customize Your Layout: Choose Your Preferred Style
         </div>
         <div className="">
-          <button className="px-4 py-2 rounded-lg bg-gray-200 font-medium duration-300 hover:bg-gray-300 mr-5">
+          <button
+            className="px-4 py-2 rounded-lg bg-gray-200 font-medium duration-300 hover:bg-gray-300 mr-5"
+            onClick={() => handleClick("regular")}
+          >
             Regular
           </button>
-          <button className="px-4 py-2 rounded-lg bg-gray-200 font-medium duration-300 hover:bg-gray-300 mr-5">
+          <button
+            className="px-4 py-2 rounded-lg bg-gray-200 font-medium duration-300 hover:bg-gray-300 mr-5"
+            onClick={() => handleClick("simple")}
+          >
             Simple
           </button>
-          <button className="px-4 py-2 rounded-lg bg-gray-200 font-medium duration-300 hover:bg-gray-300 mr-5">
+          <button
+            className="px-4 py-2 rounded-lg bg-gray-200 font-medium duration-300 hover:bg-gray-300 mr-5"
+            onClick={() => handleClick("compact")}
+          >
             Compact
           </button>
         </div>
