9e5274505da4ffc7b910888c15430e1d3a4c5ff6
James Chan <108029209+j-chany@users.noreply.github.com>
2023-02-25 21:20:24 -0800

    designed different layouts and added CSS for each

--- components/infoContainer.js
+++ components/infoContainer.js
@@ -1,33 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styles from "../styles/container.module.css";
+import { getLayout } from "@/service/utils";
+import States from "./states";
 
 const InfoContainer = (props) => {
-  const { name, emoji, emojiU, languages } = props.queryData;
-  // replace the link so we can use it as background
-  const imgLink = emojiU.replaceAll("U+", "/").replace(/\s/g, "");
+  const { name, emoji, emojiU, languages, states } = props.queryData;
+  const { router } = props;
 
-  const genLang = () => {
-    let languageList = [];
+  const genInfo = (data, styling) => {
+    let list = [];
 
-    for (let language of languages) {
-      languageList.push(<span className="mx-2">{language.name}</span>);
+    for (let ele of data) {
+      list.push(<span className={`${styles[styling]}`}>{ele.name}</span>);
     }
-
-    console.log(languageList);
-    return languageList;
+    return list;
   };
+  const [stateMode, setStateMode] = useState(false);
+  const [show, setShow] = useState(5);
+  const [pathname, setPathName] = useState(`${router.pathname}`);
+  console.log(pathname);
   return (
     <>
-      <div className="text-xl gap-3 font-bold flex flex-col items-start w-full">
-        <p>Name : {name}</p>
+      <div
+        className={` ${styles.base} ${getLayout(
+          router,
+          styles
+        )} text-sm md:text-xl lg:text-2xl w-full font-bold`}
+      >
+        <span>{name}</span>
 
-        <p>Unicode URL : {emojiU}</p>
-        <p>Languages : {genLang()}</p>
-      </div>
-      <p className="flex ">
-        <span className="duratio:300 hover:scale-150 text-9xl">
-          {emoji}
+        <span>{emojiU}</span>
+
+        <span>
+          Languages : <span>{genInfo(languages, "languages")}</span>
         </span>
-      </p>
+        <span>
+          {" "}
+          {!states.length ? (
+            <div>State: This country does not have states, try another one</div>
+          ) : (
+            <div className="flex w-full h-full flex-wrap items-center	 ">
+              <div className="mr-5">States : </div>
+              <div className="gap-2 flex flex-wrap items-center	">
+                {genInfo(states, "states").slice(0, `${show}`)}
+              </div>
+              {pathname == "/simple" ? (
+                <div className="mt-5">
+                  {" "}
+                  Please use a different view to see all states
+                </div>
+              ) : (
+                <>
+                  <button
+                    className="bg-teal-500 hover:bg-teal-600 text-white font-bold mx-2 my-2 py-2 px-2 rounded"
+                    onClick={() => {
+                      if (show <= 54) setShow(show + 5);
+                    }}
+                  >
+                    More
+                  </button>
+                  <button
+                    className="bg-teal-500 hover:bg-teal-600 text-white font-bold mx-2 my-2 py-2 px-2 rounded"
+                    onClick={() => {
+                      if (show > 5) setShow(show - 5);
+                    }}
+                  >
+                    Less
+                  </button>
+                </>
+              )}
+            </div>
+          )}
+        </span>
+        {stateMode && <States states={genInfo(states, "states")} />}
+        <p className="flex ">
+          <span className="duratio:300 hover:scale-150 text-9xl">{emoji}</span>
+        </p>
+      </div>
     </>
   );
 };
